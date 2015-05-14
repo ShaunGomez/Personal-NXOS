873c7e64bfafda40552b624f2c0b953d3905488e
ShaunGomez <shaun.gomez@gmail.com>
2015-05-14 11:37:43 -0400

    change test

---
+++ cmcast.py
@@ -0,0 +1,15 @@
+import sys
+from cisco import cli
+# v.09
+# Shaun Gomez
+#
+#clear multicast state for given group on NXOS
+#
+#
+mcast_g = str(sys.argv[1])
+cli("clear ip pim route "+mcast_g) 
+cli("clear ip mroute data-created "+mcast_g)
+cli("clear ip igmp route "+mcast_g)
+print 'All Multicast State Cleared For:'
+print mcast_g
+print 'End of Script'
