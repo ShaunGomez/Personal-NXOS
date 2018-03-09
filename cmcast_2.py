e4db082d1490a6d3909b7ee4e0417959d545a027
Shaun Gomez <shagomez@cisco.com>
2018-03-09 13:05:07 -0500

    allneighbors

--- cmcast_2.py
+++ cmcast_2.py
@@ -6,17 +6,17 @@ from cisco import clip
 #
 #clear all multicast state for given group on NXOS
 #
-#
 mcast_g = str(sys.argv[1])
+#
 cli("clear ip pim route "+mcast_g) 
 cli("clear ip mroute data-created "+mcast_g)
 cli("clear ip igmp route "+mcast_g)
 cli("clear ip igmp groups "+mcast_g)
-print 'All Multicast State Cleared For:'
-print mcast_g
-print 'Verification of State Clear'
+    print 'All Multicast State Cleared For:'
+    print mcast_g
+    print 'Verification of State Clear'
 clip("show ip mroute "+mcast_g)
 clip("show ip pim route "+mcast_g)
 clip("show ip igmp groups "+mcast_g)
 clip("show ip igmp route "+mcast_g)
-print "Verification Complete"
+    print "Verification Complete"
