90cfc073173715e735a39b3a5ddfc434b91e4d84
Shaun Gomez <shagomez@cisco.com>
2018-03-09 13:23:16 -0500

    Update cmcast
    v2.1

--- cmcast.py
+++ cmcast.py
@@ -1,15 +1,27 @@
 import sys
+import time
 from cisco import cli
-# v1.2
+from cisco import clip
+# v2.1
 # Shaun Gomez
+# John Senkow
 #
-#clear all multicast state for given group on NXOS
+# Clear all multicast state for given group on NXOS
 #
 #
 mcast_g = str(sys.argv[1])
+cli("clear ip igmp groups "+mcast_g)
 cli("clear ip pim route "+mcast_g) 
 cli("clear ip mroute data-created "+mcast_g)
-cli("clear ip igmp route "+mcast_g)
-cli("clear ip igmp groups "+mcast_g)
+cli("clear ip netstack mroute "+mcast_g)
 print 'All Multicast State Cleared For:'
 print mcast_g
+print 'Verification of State Clear, Please Wait 3 Seconds to Complete'
+for a in xrange(3,0,-1):
+	time.sleep(1)
+	print a
+clip("show ip igmp groups "+mcast_g)
+clip("show ip igmp route "+mcast_g)
+clip("show ip mroute "+mcast_g)
+clip("show ip pim route "+mcast_g)
+print "Verification Complete"
