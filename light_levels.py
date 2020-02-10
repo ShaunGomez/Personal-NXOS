efc4576b5086f8cd657fc9fc0c1f6b14adce1806
Shaun Gomez <shaun.gomez@gmail.com>
2020-02-10 14:07:13 -0500

    Update light_levels.py

--- light_levels.py
+++ light_levels.py
@@ -2,8 +2,9 @@ import sys
 from cisco import cli
 from cisco import clip
 # v1.1
-#Authors: Shaun Gomez & Predrag Zivkovic
-#Return optical light levels for a specfied interface (passed through CLI argument)
+# Authors: Shaun Gomez & Predrag Zivkovic
+# Return optical light levels for a specfied interface (passed through CLI argument)
+# Python 2.7
 eth_int = str(sys.argv[1])
     print "Returning optical light levels for " + eth_int
 cli_cmd = clip("show interface " + eth_int + " trans detail | inc Tx|Rx")
