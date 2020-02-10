a83dcf98dce90137345f0eefa1aa98b2b5c4d094
Shaun Gomez <shaun.gomez@gmail.com>
2020-02-10 14:06:30 -0500

    Update and rename cmcast.py to clear_all_multicast.py

--- clear_all_multicast.py
+++ clear_all_multicast.py
@@ -3,12 +3,9 @@ import time
 from cisco import cli
 from cisco import clip
 # v2.1
-# Shaun Gomez
-# John Senkow
-#
+# Authors: Shaun Gomez & John Senkow
 # Clear all multicast state for given group on NXOS
-#
-#
+# Python 2.7
 mcast_g = str(sys.argv[1])
 cli("clear ip igmp groups "+mcast_g)
 cli("clear ip pim route "+mcast_g) 
@@ -17,9 +14,9 @@ cli("clear ip netstack mroute "+mcast_g)
 print 'All Multicast State Cleared For:'
 print mcast_g
 print 'Verification of State Clear, Please Wait 3 Seconds to Complete'
-for a in xrange(3,0,-1):
+for verify_print in xrange(3,0,-1):
 	time.sleep(1)
-	print a
+	print verify_print
 clip("show ip igmp groups "+mcast_g)
 clip("show ip igmp route "+mcast_g)
 clip("show ip mroute "+mcast_g)
